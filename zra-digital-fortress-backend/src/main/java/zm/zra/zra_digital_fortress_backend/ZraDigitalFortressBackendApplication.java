737d6ca5c0268b08a4b32d464b3c024d61bb92fb
silas chalwe <silaschalwe@outlook.com>
2025-10-17 18:29:55 +0200

    initial

---
+++ zra-digital-fortress-backend/src/main/java/zm/zra/zra_digital_fortress_backend/ZraDigitalFortressBackendApplication.java
@@ -0,0 +1,13 @@
+package zm.zra.zra_digital_fortress_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZraDigitalFortressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZraDigitalFortressBackendApplication.class, args);
+	}
+
+}
---
+++ zra-digital-fortress-backend/src/test/java/zm/zra/zra_digital_fortress_backend/ZraDigitalFortressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zm.zra.zra_digital_fortress_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZraDigitalFortressBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
